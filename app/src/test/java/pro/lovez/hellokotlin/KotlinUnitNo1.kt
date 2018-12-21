28e366896238404b9fa37ec010f7e52d0900aca3
lovez <blue@lovez.pro>
2018-12-21 16:12:02 +0800

    Hello Kotlin Base

---
+++ app/src/test/java/pro/lovez/hellokotlin/KotlinUnitNo1.kt
@@ -0,0 +1,31 @@
+package pro.lovez.hellokotlin
+
+/**
+ * 基础 <br/>
+ *
+ * 1.基础类型
+ *
+ */
+class KotlinUnitNo1{
+
+
+    companion object {
+        var double:Double = 1.0
+        var float:Float = 1.0f
+        var long:Long = 1L
+        var int:Int = 1
+        var short:Short = 1
+        var byte:Byte = 1
+
+        fun baseType(){
+            System.out.println("type\tBitWidth\tMin\tMax")
+            System.out.println("${double}\t64\t")
+            System.out.println("dobule\t64")
+            System.out.println("dobule\t64")
+            System.out.println("dobule\t64")
+
+        }
+
+    }
+
+}
